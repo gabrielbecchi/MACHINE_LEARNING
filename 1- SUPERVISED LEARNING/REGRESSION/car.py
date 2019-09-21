5c92893f53f51fac0b864b4a95099d718b809c73
becchi <gabrielbecchi97@gmail.com>
2019-09-21 12:47:39 -0700

    Regression methods

--- 1- SUPERVISED LEARNING/REGRESSION/car.py
+++ 1- SUPERVISED LEARNING/REGRESSION/car.py
@@ -2,8 +2,8 @@ from prepareData import prepareData
 from crossValidation import crossValidation
 import pandas as p
 import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import Ridge
+from sklearn import linear_model
+import numpy as np
 
 columns = ["symboling","losses","make","fuel-type","aspiration",
 	"num-of-doors","body-style","drive-wheels","engine-location",
@@ -19,7 +19,20 @@ df = p.read_csv("DATA/imports_85_reformed.csv")
 kfold = 5
 cv = crossValidation(df,kfold)
 
-for i in range(kfold):
-	train, test = cv.getNextSet()
-	input(train)
+machine = linear_model.LinearRegression()
+print("Ordinary Least Squares - "+str(round(cv.runTest(machine,"price"),3)))
 
+for alpha in np.arange(0.1,1,0.1):
+	machine = linear_model.Ridge(alpha=alpha)
+	print("Ridge (alpha: "+str(round(alpha,1))+") - "+\
+		str(round(cv.runTest(machine,"price"),3)))
+
+for alpha in np.arange(0.1,1,0.1):
+	machine = linear_model.Lasso(alpha=alpha,tol=0.1)
+	print("Lasso (alpha: "+str(round(alpha,1))+") - "+\
+		str(round(cv.runTest(machine,"price"),3)))
+
+for alpha in np.arange(0.1,1,0.1):
+	machine = linear_model.ElasticNet(alpha=alpha,tol=0.1)
+	print("ElasticNet (alpha: "+str(round(alpha,1))+") - "+\
+		str(round(cv.runTest(machine,"price"),3)))
--- 1- SUPERVISED LEARNING/REGRESSION/crossValidation.py
+++ 1- SUPERVISED LEARNING/REGRESSION/crossValidation.py
@@ -14,6 +14,7 @@ class crossValidation:
 			indexList = list(set(indexList).difference(set(self.listCV[i])))
 		self.listCV.append(indexList)
 		self.currentFold = 0;
+		self.meanSquaredError = []
 
 	def getNextSet(self):
 		test = self.df.loc[self.listCV[self.currentFold],:]
@@ -21,3 +22,25 @@ class crossValidation:
 		train = self.df.loc[trainIndex,:]
 		self.currentFold = (self.currentFold+1)%self.kfolds
 		return train.reset_index(),test.reset_index()
+
+	def addResults(self,labelActual,labelPredicted):
+		labelActual = list(labelActual.values)
+		sumSquares = 0
+		for i in range(len(labelActual)):
+			#print(str(labelActual[i])+" - "+str(labelPredicted[i]))
+			sumSquares += (labelActual[i]-labelPredicted[i])**2
+		self.meanSquaredError.append((sumSquares/len(labelActual))**0.5)
+		#input()
+
+	def rootMeanSquaredError(self):
+		score = (sum(self.meanSquaredError)/len(self.meanSquaredError))
+		self.meanSquaredError = []
+		return score
+
+	def runTest(self,machine,labelColumn):
+		for i in range(self.kfolds):
+			train, test = self.getNextSet()
+			machine.fit(train.drop(columns=[labelColumn]), train[labelColumn])
+			predicted = machine.predict(test.drop(columns=[labelColumn]))
+			self.addResults(test["price"],predicted)
+		return self.rootMeanSquaredError()
