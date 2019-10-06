6af268ed9551608868f454d3e078d045a47b5a80
becchi <gabrielbecchi97@gmail.com>
2019-10-05 18:49:17 -0700

    Add KNN and iris data

--- 1- SUPERVISED LEARNING/CLASSIFICATION/SVM/iris.py
+++ 1- SUPERVISED LEARNING/CLASSIFICATION/SVM/iris.py
@@ -10,15 +10,7 @@ warnings.filterwarnings("ignore")
 #p.set_option('display.max_columns', 500)
 #p.set_option('display.width', 1000)
 
-name = ['index','label'] + [str(i) for i in list(np.arange(0,30))]
-df = p.read_csv("DATA/cancer.csv",names=name, index_col='index')
-df_label = df['label']
-df = df.drop('label',1)
-df = (df-df.min())/(df.max()-df.min())
-df['label'] = df_label
-
-df.loc[df['label'] == 'B', 'label'] = 0
-df.loc[df['label'] == 'M', 'label'] = 1
+df = p.read_csv("DATA/iris.csv",names=[0,1,2,3,'label'])
 labelColumn = 'label'
 
 cv = crossValidation(df,5)
